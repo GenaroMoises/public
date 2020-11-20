5f0fe1379625009b07eb24b08c36dedf3b1f246a
GenaroMoises <genaromoises@gmail.com>
2020-11-20 16:35:28 -0500

    actualizacion de json

--- js/js.js
+++ js/js.js
@@ -34,107 +34,29 @@ function botonNota(){
 
 }
 
-/*function btn(){
-    document.getElementById('boton').onclick;
-    let titulo = document.getElementById('titulo');
-    if(titulo){
-        titulo: document.getElementById('titulo').classList.add('text-primary')
-    }
-}
-function btn1(){
-    document.getElementById('boton1');
-    let titulo = document.getElementById('titulo');
-    if(titulo){
-        titulo: document.getElementById('titulo').classList.add('text-success')
-    }
-}
-function btn2(){
-    document.getElementById('boton2');
-    let titulo = document.getElementById('titulo');
-    if(titulo){
-        titulo: document.getElementById('titulo').classList.add('text-warning')
-    }
-}*/
-/*
-const tabla = document.querySelector('#lista-usuarios tbody');
 
-function cargarUsuarios(){
-    fetch('dato.json')
-        .then(respuesta => respuesta.json()) //Indicamos el formato en que se desea obtener la información
-        .then(usuarios => {
-            usuarios.forEach(usuario => {
-                const row = document.createElement('tr');
-                row.innerHTML += `
-                    <td>${usuario.titulo}</td>
-                    <td>${usuario.artista}</td>
+console.log('hola mundo');
 
-                    <td>${usuario.email}</td>
-                    <td>${usuario.company.name}</td>
-                `;
-                tabla.appendChild(row);                
-            });
-        }) // Aquí mostramos dicha información
-        .catch(error => console.log('Hubo un error : ' + error.message))
-}
-
-cargarUsuarios();*/
-/************************************************** */
-/*var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function(){
-    if(this.readyState ==4 && this.status==200){
-
-    }
-}*/
-
-/***************************** */
-
-/*function cargarJson(){
-    fetch('dato.json')
-    .then(function(res){
-        return res.json();
+//document.getElementById('button2'):
+//addEventListener('click',loadjson);
+function loadjson(){
+    fetch('datos.json')
+    .then(function(response){
+        return response.json();
     })
     .then(function(data){
-        let html ='';
-        data.forEach(dato) {
+        let html='';
+        data.forEach(function(datos){
             html +=`
-                <li>${titulo} ${artista}</li>
-            `;
+            <tr>
+                <th scope="row">${datos.Id}</th>
+                <td>${datos.Proyecto}</td>
+                <td>${datos.Link}</td>
+            </tr>
+            `
         })
-        document.getElementById('resultado').innerHTML = html:
-    })*/
+        document.getElementById('result').innerHTML = html;
+    })
+}
 
-    /***https://www.youtube.com/watch?v=xqBvtvXh9Z4 */
 
-    /**************https://www.w3schools.com/js/tryit.asp?filename=tryjs_events1 */
-
-    var contenido = document.querySelector('#contenido')
-
-        function traer() {
-            fetch('datos.json')
-                .then(res => res.json())
-                .then(datos => {
-                    //console.log(datos)
-                    tabla(datos)
-                })
-        }
-
-        function tabla(datos) {
-            // console.log(datos)
-            contenido.innerHTML = ''
-            for(let valor of datos){
-                // console.log(valor.nombre)
-                contenido.innerHTML += `
-                
-                <tr>
-                    <th scope="row">${ valor.id }</th>
-                    <td>${ valor.titulo }</td>
-                    <td>${ valor.artista }</td>
-                    <td>${ valor.estado ? "Activo" : "Eliminado" }</td>
-                </tr>
-                
-                `
-            }
-        }
-
-        /***https://www.youtube.com/watch?v=M4LaQ3KUGOM */
-        /**https://drive.google.com/drive/u/0/folders/10p-k0atMXl03jfIRbwpfKyF1lc9P9Eq5 */
